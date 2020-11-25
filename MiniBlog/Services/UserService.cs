5c57364cd7ec2b145c85c678d60c7e85485fbafc
Lei ZHU <lzhu19@slb.com>
2020-11-25 19:19:28 +0800

    rafactor:extract UserGetAll and UserUpdate method into UserService Class

---
+++ MiniBlog/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MiniBlog.Model;
+using MiniBlog.Stores;
+
+namespace MiniBlog.Services
+{
+    public class UserService
+    {
+        private readonly IUserStore userStore;
+        public UserService(IUserStore userStore)
+        {
+            this.userStore = userStore;
+        }
+
+        public void UserRegister(User user)
+        {
+            if (!userStore.Users.Exists(_ => user.Name.ToLower() == _.Name.ToLower()))
+            {
+                userStore.Users.Add(user);
+            }
+        }
+
+        public List<User> UserGetAll()
+        {
+            return userStore.Users;
+        }
+
+        public User UserUpdate(User user)
+        {
+            var foundUser = userStore.Users.FirstOrDefault(_ => _.Name == user.Name);
+            if (foundUser != null)
+            {
+                foundUser.Email = user.Email;
+            }
+
+            return foundUser;
+        }
+    }
+}
